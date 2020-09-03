b8424cd2a48024bbbccd46266ce9157148d6bb08
Sam Li <pyrosamatic@gmail.com>
2020-09-03 20:37:10 +1000

    Can detect number of cars passing through left lane

--- read_saved_data.py
+++ read_saved_data.py
@@ -11,37 +11,96 @@ import time
 
 #Flag to pause
 pause = 0
+left = 0
+framesPassed = 0
 
 def handle_close(event):
     exit()
 
 def press(event):
 	global pause
-	if event.key:
+	if event:
 		pause = 1
 
 def tlvHeaderDecode(data):
 	tlvType, tlvLength = struct.unpack('2I', data)
 	return tlvType, tlvLength
 
+#9 cars passed in 20 senconds left lane
+
+def filter_cars(objects):
+	global left
+	global framesPassed
+	counter = 0
+	#Filter out stationary objects
+	for i in range(len(objects)):
+		if objects[counter][3] == 0:
+			objects.pop(counter)
+		else:
+			counter += 1
+
+	objects.sort(key=lambda x: x[0])
+	leftLane = []
+	rightLane = []
+	for i in range(len(objects)):
+		if objects[i][3] > 0:
+			leftLane.append(objects[i])
+		elif objects[i][0] < 0:
+			rightLane.append(objects[i])
+	count = 0
+	leftLane.sort(key=lambda x: x[1])
+	for i in range(len(leftLane)):
+		if leftLane[i][1] < 10:
+			count += 1
+	for i in reversed(range(1, count)):
+		leftLane.pop(i)
+	
+	objects = leftLane + rightLane
+	display = objects
+	for i in range(len(display)):
+		display[i][0] = round(display[i][0], 2)
+		display[i][1] = round(display[i][1], 2)
+		display[i][2] = round(display[i][2], 2)
+		display[i][3] = round(display[i][3], 2)
+	print(display)
+
+	for i in range(len(leftLane)):
+		if leftLane[i][1] < 10:
+			if framesPassed == 0 or framesPassed > 10:
+				left += 1
+				framesPassed = 0
+	framesPassed += 1
+
+	print("NUM OF LEFT CARS:", left)
+	
+
+	return objects
+
+
 def parseDetectedObjects(data, tlvLength, ax, timestamp):
 	global pause
-	objects = [[], [], [], []]
+	global left
+	objects = []
 	for i in range(0, tlvLength, 16):
 		x, y, z, vel = struct.unpack('4f', data[i:i + 16])
-		objects[0].append(x)
-		objects[1].append(y)
-		objects[2].append(z)
-		objects[3].append(vel)
-		print("x =", x, "y =", y, "z =", z,"vel =", vel)
+		objects.append([x, y, z, vel])
 	ax.clear()
 	ax.axis([-25, 25, 0, 50])
-	ax.plot(objects[0], objects[1], 'bo', markersize=3)
+	for n in filter_cars(objects):
+		ax.plot(n[0], n[1], 'bo', markersize=3)
+	# for n in objects:
+	# 	if n[3] != 0:
+	# 		print("x =", n[0], "y =", n[1], "z =", n[2],"vel =", vel)
+	# 		ax.plot(n[0], n[1], 'bo', markersize=3)
 	plt.text(0.5, 0.95, "Timestamp: " + str(timestamp), 
+			horizontalalignment='center', verticalalignment='center', 
+			transform=ax.transAxes)
+	plt.text(0.2, 0.7, "Cars pass in left lane: " + str(left), 
 			horizontalalignment='center', verticalalignment='center', 
 			transform=ax.transAxes)
 	plt.pause(0.1)
 	plt.draw()
+	#press(1)
 	if pause == 1:
 		pause = 0
 		plt.waitforbuttonpress()
@@ -64,7 +123,7 @@ def tlvHeader(data, ax):
 		print("Frame Number: ", counter)
 		print("Timestamp:", timestamp)
 		print("Detect Obj:\t%d "%(numObj))
-		if version > 0x01000005:
+		if version > 0x01000005: 
 			headerLength = 40
 			pendingBytes = length - headerLength
 			data = data[headerLength:]
