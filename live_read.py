204e4f39c3e4998c4f17705fa5d937c95a246ebe
Sam Li <pyrosamatic@gmail.com>
2020-10-07 22:56:37 +1000

    Implementing live reading

--- live_read.py
+++ live_read.py
@@ -1,42 +1,25 @@
-import struct
-import sys
-import math
-import datetime
-import matplotlib.pyplot as plt
+import serial
 import numpy as np
+import matplotlib.pyplot as plt
+import sys
 import time
+import datetime
+import math
+import struct
 
-# Note: 
-# In plot = 1 and step = 0 mode:
-# Press any key to pause plot and click any key again to step through
-# Press mouse button to unpause
-
-# In plot = 1 and step = 1 mode:
-# Press any key to step through the plot
-
-# In plot = 0 mode:
-# Program runs analysis much faster without plot
-# Look at terminal for results
-
-#Flags
-pause = 0
 left = 0
 right = 0
 framesPassedLeft = 0
 framesPassedRight = 0
-plot = 1
-step = 0
 
 def handle_close(event):
-    exit()
-
-def press(event):
-	global pause
-	if event:
-		pause = 1
+	exit()
 
 def tlvHeaderDecode(data):
-	tlvType, tlvLength = struct.unpack('2I', data)
+	try:
+		tlvType, tlvLength = struct.unpack('2I', data)
+	except:
+		return -1, - 1
 	return tlvType, tlvLength
 
 #9 cars passed in 20 senconds left lane
@@ -110,37 +93,31 @@ def filter_cars(objects):
 
 
 def parseDetectedObjects(data, tlvLength, ax, timestamp):
-	global pause, left, right
+	global left, right
 	objects = []
 	for i in range(0, tlvLength, 16):
 		x, y, z, vel = struct.unpack('4f', data[i:i + 16])
 		objects.append([x, y, z, vel])
-	if plot:
 		ax.clear()
 		ax.axis([-25, 25, 0, 50])
-		for n in filter_cars(objects):
-			if n[3] > 0:
-				ax.plot(n[0], n[1], 'bo', markersize=3)
-			if n[3] < 0:
-				ax.plot(n[0], n[1], 'ro', markersize=3)
-		plt.text(0.5, 0.95, "Timestamp: " + str(timestamp), 
-				horizontalalignment='center', verticalalignment='center', 
-				transform=ax.transAxes)
-		plt.text(0.2, 0.7, "Cars pass in left lane: " + str(left), 
-				horizontalalignment='center', verticalalignment='center', 
-				transform=ax.transAxes)
-		plt.text(0.2, 0.6, "Cars pass in right lane: " + str(right), 
-				horizontalalignment='center', verticalalignment='center', 
-				transform=ax.transAxes)
+		# for n in filter_cars(objects):
+		# 	if n[3] > 0:
+		# 		ax.plot(n[0], n[1], 'bo', markersize=3)
+		# 	if n[3] < 0:
+		# 		ax.plot(n[0], n[1], 'ro', markersize=3)
+		# plt.text(0.5, 0.95, "Timestamp: " + str(timestamp), 
+		# 		horizontalalignment='center', verticalalignment='center', 
+		# 		transform=ax.transAxes)
+		# plt.text(0.2, 0.7, "Cars pass in left lane: " + str(left), 
+		# 		horizontalalignment='center', verticalalignment='center', 
+		# 		transform=ax.transAxes)
+		# plt.text(0.2, 0.6, "Cars pass in right lane: " + str(right), 
+		# 		horizontalalignment='center', verticalalignment='center', 
+		# 		transform=ax.transAxes)
+		for n in objects:
+			ax.plot(n[0], n[1], 'bo', markersize=3)
 		plt.pause(0.1)
 		plt.draw()
-	else:
-		filter_cars(objects)
-	if plot and step:
-		press(1)
-	if pause == 1:
-		pause = 0
-		plt.waitforbuttonpress()
 
 def tlvHeader(data, ax):
 	counter = 0
@@ -157,15 +134,17 @@ def tlvHeader(data, ax):
 			timestamp = timestamp[:9]
 		else:
 			timestamp = timestamp + ".0"
-		print("Frame Number: ", counter)
-		print("Timestamp:", timestamp)
-		print("Detect Obj:\t%d "%(numObj))
+		# print("Frame Number: ", counter)
+		# print("Timestamp:", timestamp)
+		# print("Detect Obj:\t%d "%(numObj))
 		if version > 0x01000005: 
 			headerLength = 40
 			pendingBytes = length - headerLength
 			data = data[headerLength:]
 			for i in range(numTLVs):
 				tlvType, tlvLength = tlvHeaderDecode(data[:8])
+				if tlvType == -1 and tlvLength == -1:
+					return
 				data = data[8:]
 				if (tlvType == 1):
 					parseDetectedObjects(data, tlvLength, ax, timestamp)
@@ -173,44 +152,34 @@ def tlvHeader(data, ax):
 				pendingBytes -= (8+tlvLength)
 			data = data[pendingBytes:]
 
-def argument_check(argv):
-	global plot, step
-	if not(len(argv) == 2 or len(argv) == 4):
-		print("Usage: read_saved_data.py inputFile.bin --optional [plot step]")
-		sys.exit()
-	if len(argv) == 4:
-		try:
-			plot = int(argv[2])
-			step = int(argv[3])
-			if not((plot == 0 or plot == 1) and (step == 0 or step == 1)):
-				raise ValueError()
-		except Exception as e:
-			print("Invalid plot or step value")
-			sys.exit()
-
 if __name__ == "__main__":
-	argument_check(sys.argv)
-	#Open data file for reading
-	fileName = sys.argv[1]
+	configFile = sys.argv[1]
+	# Windows
 	try:
-		rawDataFile = open(fileName, "rb")
+		CLIport = serial.Serial('COM4', 115200)
+		dataPort = serial.Serial('COM3', 921600)
 	except:
-		print("Invalid data file")
-		sys.exit()
-	rawData = rawDataFile.read()
-	rawDataFile.close()
+		print("Can't connect to serial ports.")
+		exit()
+	# Send configuration to device
+	config = [line.rstrip('\r\n') for line in open(configFile)]
+	for i in config:
+		CLIport.write((i+'\n').encode())
+		print(i)
+		time.sleep(0.01)
+
 	magic = b'\x02\x01\x04\x03\x06\x05\x08\x07'
-	offset = rawData.find(magic)
-	rawData = rawData[offset:]
 
-	#Generate plot
-	if plot:
-		plt.ion()
-		fig, ax = plt.subplots()
-		fig.canvas.mpl_connect('close_event', handle_close)
-		fig.canvas.mpl_connect('key_press_event', press)
-	else:
-		#Dummy variable
-		ax = 1
+	plt.ion()
+	fig, ax = plt.subplots()
+	fig.canvas.mpl_connect('close_event', handle_close)
 
-	tlvHeader(rawData, ax)
+	while 1:
+		data = dataPort.read(dataPort.in_waiting)
+		if data:
+			offset = data.find(magic)
+			if offset != -1:
+				data = data[offset:]
+				tlvHeader(data, ax)
+
+
